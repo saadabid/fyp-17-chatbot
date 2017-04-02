fbf2eab6b4967aba2863c2efd9da0bcc207a04db
M-SAAD\Saad <saadabid15@gmail.com>
2017-04-03 01:18:26 +0500

    edit q/a index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // parameters
-$hubVerifyToken = 'TOKEN123456abcd';
+$hubVerifyToken = 'TOKEN1234GGGSS';
 $accessToken = "EAAQ8poRtX3QBADlLXhsd3PZAZADGmDXdZAZAa5EPt41u83ZBHWoITmJ3nqpfLGSGJFQVJA3gE5TfEvkiNfSM9ZCVVqZBq85PtZC1WQHa0aheGQDs4JdKnwaYC8M4eVB1TI5XWsKQH6juioZC1Yreoj5sVcUUwgEboeg1yjbHnLRw1iAZDZD";
 
 // check token at setup
@@ -17,8 +17,21 @@ $messageText = $input['entry'][0]['messaging'][0]['message']['text'];
 
 
 $answer = "I don't understand. Ask me 'hi'.";
-if($messageText == "hi") {
-    $answer = "Hello";
+if($messageText == "hi") 
+{
+    $answer = "Hello sir, How may I help you ?";
+}
+if($messageText == "I want to buy wallet")
+{
+	$answer = "Please mention the product code";
+}
+if($messageText == "GSW-501")
+{
+	$answer = "Product price: Rs 1200";
+}
+if($messageText == "How to order?")
+{
+	$answer = "To book your order mention product code, color, name, contact number and complete address.";
 }
 
 $response = [
